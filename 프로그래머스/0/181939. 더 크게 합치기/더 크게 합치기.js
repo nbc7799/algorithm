bf70bdd1ee085797071fe2f06762430f599ca9b5
Journey <80373922+nbc7799@users.noreply.github.com>
2025-07-22 10:40:24 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,13 +1,3 @@
 function solution(a, b) {
-const fir = String(a)
-const sec = String(b)
-const sum1 = Number(fir+sec)
-const sum2 = Number(sec+fir)
- if(sum1 > sum2) {
-    return sum1   
- } else if(sum1 < sum2){
-     return sum2
- }
-
-    return sum1
+return Math.max(Number(`${a}${b}`), Number(`${b}${a}`))
 }
