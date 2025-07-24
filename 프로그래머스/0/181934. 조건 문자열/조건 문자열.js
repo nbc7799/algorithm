60a4cd62af27f2c4af84284b50618d64d3029aa4
Journey <80373922+nbc7799@users.noreply.github.com>
2025-07-24 10:40:23 +0900

    [level 0] Title: 조건 문자열, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,13 @@
+function solution(ineq, eq, n, m) {
+    
+    if (ineq === ">" && eq === "="){
+        return n >= m ? 1 : 0
+    } else if(ineq === "<" && eq === "="){
+        return n <= m ? 1:0
+    } else if(ineq === ">" && eq === "!"){
+        return n > m ? 1:0
+        
+    } else if(ineq === "<" && eq === "!") {
+        return n <m ? 1 : 0
+    }
+}
