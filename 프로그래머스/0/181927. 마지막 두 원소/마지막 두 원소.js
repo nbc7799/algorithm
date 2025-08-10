27ac7ec23f005feaf15a09da886de70b8c5e86e4
Journey <80373922+nbc7799@users.noreply.github.com>
2025-08-11 02:01:14 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,11 +1,6 @@
 function solution(num_list) {
-    const last = num_list[num_list.length -1]
-     const prev = num_list[num_list.length -2]
+    const a = num_list.at(-1)
+    const b = num_list.at(-2)
     
-    if(last > prev) {
-        num_list.push(last - prev);
-    } else {
-        num_list.push(last *2)
-    }
-    return num_list
+    return [...num_list, a > b ? a-b : a * 2]
 }
